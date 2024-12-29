03828f0de1ac2613193f018baa4ad922ce761b25
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2024-12-30 05:20:58 +0530

    Update database.js

--- src/components/backend/database.js
+++ src/components/backend/database.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const DATA_FILE = path.join(__dirname, './users.json');
 
 // Middleware
